8dfb201877cfc037fa64e6288e9a375e7351f110
Tsabitah59 <tsabitahsafa06@gmail.com>
2025-05-15 15:04:57 +0700

    finishing bnext product section

---
+++ bnext_fe/lib/features/dashboard/presentation/dashboard/components/components.dart
@@ -0,0 +1,7 @@
+export 'app_bar.dart';
+export 'banner_widget.dart';
+export 'mini_slider_widget.dart';
+export 'potrait_slider_widget.dart';
+export 'product_card.dart';
+export 'video_app_iitem.dart';
+export 'video_slider_widget.dart';

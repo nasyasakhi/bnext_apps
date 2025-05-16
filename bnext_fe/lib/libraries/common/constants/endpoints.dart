eadaec97879772716ff0ef14d72a85eb53197f65
tsayoo <tsaljzahidah@gmail.com>
2025-05-16 17:27:34 +0700

    Learning about libraries - common - constant folder

--- bnext_fe/lib/libraries/common/constants/endpoints.dart
+++ bnext_fe/lib/libraries/common/constants/endpoints.dart
@@ -1,14 +1,18 @@
+// Buat nampung semua endpoint API URL, jadi kode lebih rapi tanpa URL di setiap file nya.
 class Endpoints {
   /* BASE URL */
 
   static const apiLogin = 'http://38.47.90.76:8202';
   static const apiUser = 'http://38.47.90.76:8203/api/user';
+  // dipisah jadi dua karena kayaknya login dan user info beda port/backend service.
 
   /* AUTH */
   static const login = '$apiLogin/login';
   static const register = '$apiUser/register';
   static const verifOtp = '$apiUser/verifyOtp';
   static const sendOtp = '$apiUser/sendOtp';
+  // buat endpoint.
+  // pake $apiUser/$apiLogin biar gampang update kalo misalnya base URL-nya berubah.
 
   /* USER */
   static const profile = '$apiUser/userProfile';
--- bnext_fe/lib/libraries/common/constants/validators.dart
+++ bnext_fe/lib/libraries/common/constants/validators.dart
@@ -1,9 +1,27 @@
 class Validators {
+  // kode buat ngecek regex email. harus ada @, domain valid dan gak asal asalan.
+  // disimpen sebagai final biar dibuat sekali dan dipake berkali kali
   static final RegExp _emailRegExp = RegExp(
     r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$',
   );
 
   static isValidEmail(String email) {
+    // Buat ngecek email, cocok gak sama regex diatas
     return _emailRegExp.hasMatch(email);
   }
+  // Regex adalah singkatan dari Regular Expression alias pola atau rumus buat nyari atau nyocokin teks.
+  /* jadi kalo diperhatiin = 
+  1. r'.....' buat ngasih tau dart, kalo teks didalam r ini ga perlu dibaca sistem. kalo ga dikasih r, nanti sistem bisa error karna mikir \ nya sebagai pemisah karakter. r = raw (kata GPT)
+  2. ^ buat ngecek harus dimulai dgn string.
+  3. a-z = boleh pake smua huruf alphabet kecil
+  4. A-Z = boleh pake smua huruf alphabet besar
+  5. 0-9 = boleh pake smua angka, dari 0-9
+  6. .!#$%&’*+/=?^_{|}~- = simbol simbol yang boleh dipake di email.
+  7. + @ = harus ada @ nya. @ itu simbol domain.
+  8. [a-zA-Z0-9-]+ = stelah domain, boleh pake huruf, angka. (@gmail, @yahoo)
+  9. (?:...) = kayak pengelompokan sementara. tapi ga nyimpen sesuatu.
+  10. \. = titik literal, jadi wajib ada kayak @gmail.com, @yahoo.co.id, @idn.sch.id.
+  11 [a-zA-Z0-9-] = Setelah titik, harus ada domain yang isinya huruf, angka, atau tanda strip.
+  12. * setelah titik, boleh ga ada atau malah lebih dari 1. (@domain @gmail.com @idn.sch.id).
+  */
 }
